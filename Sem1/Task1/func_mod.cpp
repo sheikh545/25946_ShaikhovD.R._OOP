0aa135d644cbd05363d0d737e057af8bc4325400
Damir Shaikhov <d.shaikhov@g.nsu.ru>
2026-09-16 11:50:15 +0300

    Task1 - cpp and h files

---
+++ Sem1/Task1/func_mod.cpp
@@ -0,0 +1,24 @@
+#include "Header2.h"
+#include <random>
+
+namespace func2
+{
+	template <typename T>
+	T func_average(const T &a, const T &b)
+	{
+		T aver_sum = (a + b) / 2;
+
+		std::random_device rd;
+		std::mt19937 generator(rd()); // rd() = seed
+		// generator - источник случайности для распределения
+
+		std::uniform_int_distribution<int> chance(0, 1);
+		std::uniform_int_distribution<int> random_number(1, 100);
+
+		if (chance(generator)) return aver_sum + random_number(generator);
+		else return aver_sum;
+	}
+
+	template int func_average<int>(const int& a, const int& b);
+	template double func_average<double>(const double& a, const double& b);
+}
